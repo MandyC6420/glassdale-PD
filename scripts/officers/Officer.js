81a02db0febeeba30142f095fedebc54a2009d5d
Mandy <drgnfli6420@yahoo.com>
2021-01-25 16:48:49 -0500

    csswhereareyou

--- scripts/officers/Officer.js
+++ scripts/officers/Officer.js
@@ -1,4 +1,4 @@
 export const officer = (officerObject) => {
-    return `<section class = "officer-card"> <h1>Officer Name: ${officerObject.name}</h1>
+    return `<section class = "officer-card"><h1>Officer Name: ${officerObject.name}</h1>
     </section>`;
   };
