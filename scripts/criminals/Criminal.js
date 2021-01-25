fdc6bddc058df2aa05dba59c5ac2774413692fe4
Mandy <drgnfli6420@yahoo.com>
2021-01-25 11:18:57 -0500

    added html representation string interpolation

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,7 +1,12 @@
-let criminals = []
-
-export const useCriminals = () => criminals.slice()
-
-export const getCriminals = () => {
-   
+export const criminal = (criminalObject) => {
+   return `<h2>${criminalObject.name}</h2>
+        <ul>
+            <li>${criminalObject.age}</li>
+            <li>${criminalObject.conviction}</li>
+            <li>${criminalObject.incarceration}</li>
+            <li>${Date(criminalObject.incarceration.start).toLocaleDateString('en-US')}</li>
+            <li>${Date(criminalObject.incarceration.end).toLocaleDateString('en-US')}</li>
+            
+       </ul>`
+        
 }
