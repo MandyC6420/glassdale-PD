505c478180ceb4fd5b49bd404a319e354996680d
Mandy <drgnfli6420@yahoo.com>
2021-01-26 11:22:11 -0500

    trying to get CSS to appear consistently

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,26 @@
-console.log("Welcome to the main module")
+console.log("Welcome to the main module");
 
-import { CriminalList } from "./criminals/CriminalList.js"
+import { CriminalList } from "./criminals/CriminalList.js";
 
 // CriminalList()
 
-import { OfficerList } from "./officers/OfficerList.js"
+import { OfficerList } from "./officers/OfficerList.js";
 
 // OfficerList()
 
-import { ConvictionList } from "./convictions/convictionList.js"
+import { ConvictionList } from "./convictions/convictionList.js";
 
-ConvictionList()
+ConvictionList();
 
-import { FacilityList } from "./facility/facilityList.js"
+import { FacilityList } from "./facility/facilityList.js";
 
 // FacilityList()
 
-const darkModeButton = document.querySelector("#dark-mode")
-darkModeButton.addEventListener("click", function(){
-    // Select the entire body tag
-  const bodyElement = document.querySelector("body")
+const darkModeButton = document.querySelector("#dark-mode");
+darkModeButton.addEventListener("click", function () {
+  // Select the entire body tag
+  const bodyElement = document.querySelector("body");
 
   // Add a class
-  bodyElement.classList.toggle("dark-background")
-})
+  bodyElement.classList.toggle("dark-background");
+});
