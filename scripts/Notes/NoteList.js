7d952f0e15b96093aefdf7981ba94cbebbd4ff38
Mandy <drgnfli6420@yahoo.com>
2021-01-28 15:38:39 -0500

    note input displaying, no notes

---
+++ scripts/Notes/NoteList.js
@@ -0,0 +1,42 @@
+import { getNotes, saveNote } from "./NoteDataProvider.js";
+import { note } from "./Note.js";
+
+export const NoteList = () => {
+  getNotes().then(() => {
+    let noteContainer = document.querySelector(
+      ".noteFormContainer"
+    );
+    let allTheNotes = saveNote();
+
+    let NoteListHTML = "";
+
+    for (let i = 0; i < allTheNotes.length; i++) {
+      noteListHTML += note(allThenotes[i]);
+    }
+
+    noteContainer.innerHTML = noteListHTML;
+
+    console.log(noteListHTML);
+
+    // Now that you have the data, what component should be rendered?
+  });
+};
+
+// Handle browser-generated click event in component
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+
+        // Make a new object representation of a note
+        const newNote = {
+            // Key/value pairs here
+        }
+
+        // Change API state and application state
+        saveNote(newNote)
+        .then(NoteList) // Refresh your list of notes once you've saved your new one
+    }
+})
+
+const NoteForm = () => {
+    // rest of the code here
+}
--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -41,10 +41,10 @@ const render = (convictionsCollection) => {
 // This won't throw an error, but it will fire any time there's a change event anywhere in the main container
 const eventHub = document.querySelector("main");
 eventHub.addEventListener("change", (eventObject) => {
-  console.log("You clicked somewhere in the main container");
+  // console.log("You clicked somewhere in the main container");
 
   // To be more specific, we need to know specifically what we clicked on
-  console.log("Here is the element you clicked on: ", eventObject.target);
+  // console.log("Here is the element you clicked on: ", eventObject.target);
 
   if (eventObject.target.id === "crimeSelect") {
     //  console.log("You selected something from the crime dropdown");
