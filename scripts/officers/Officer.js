5785daea4d64e55bda9eb6950c5e10fd5873a044
Mandy <drgnfli6420@yahoo.com>
2021-02-10 16:12:44 -0500

    criminal dropdown printing criminal names

--- scripts/officers/Officer.js
+++
@@ -1,6 +0,0 @@
-export const officer = (officerObject) => {
-  return `<div class="card w-60 text-center"><section class = "officer-card"><h1>Officer Name: ${officerObject.name}</h1>
-    </section>`;
-};
-
-
--- scripts/witnesses/Witness.js
+++
@@ -1,8 +0,0 @@
-export const witness = (witnessObject) => {
-  return `<div class="card w-60 text-center"><section class="witness-card">
-    <h3 class="witness-name">Witness Name:
-    ${witnessObject.name}
-    </h3>
-    Witness Statement:<p>${witnessObject.statements}</p>
-    </div>`;
-};
