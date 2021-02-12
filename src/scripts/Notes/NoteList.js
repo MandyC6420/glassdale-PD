ed1f9132b0b64c77f2fb82f54d9f70925235b969
Mandy <drgnfli6420@yahoo.com>
2021-02-12 18:02:40 -0500

    notes work, delete works, but I can't get the suspect name to link

--- src/scripts/Notes/NoteList.js
+++ src/scripts/Notes/NoteList.js
@@ -1,22 +1,40 @@
-import { getNotes, useNotes } from "./NoteDataProvider.js";
+import { getNotes, useNotes, deleteNote } from "./NoteDataProvider.js";
 import { note } from "./Note.js";
+import { getCriminals, useCriminals,} from "../criminals/CriminalDataProvider.js";
+
+const eventHub = document.querySelector(".target-container")
+let contentTarget = document.querySelector(".target-container");
 
 export const NoteList = () => {
-  // clear the container
-  document.querySelector(".noteContainer").innerHTML = "";
-  getNotes().then(() => {
-    let noteContainer = document.querySelector("#note-Form-Container");
-    let allTheNotes = useNotes();
-
-    let noteListHTML = "";
-
-    for (let i = 0; i < allTheNotes.length; i++) {
-      noteListHTML += note(allTheNotes[i]);
-    }
-
-    noteContainer.innerHTML = noteListHTML;
-  });
+  
+  getNotes()
+    .then(getCriminals())
+    .then(() => {
+      let allTheNotes = useNotes();
+      let allTheCriminals = useCriminals();
+      
+      const arrayofMatchingNotes = allTheNotes.map((singleNote) => {
+        const relatedCriminal = allTheCriminals.find(criminal => criminal.id === +singleNote.criminalId
+        );
+        const notesHTML = note(singleNote, relatedCriminal)
+        return notesHTML;
+      });
+      
+      // console.log(notesHTML);
+      const HTMLstring = arrayofMatchingNotes.join("");
+      contentTarget.innerHTML = HTMLstring
+    });
 };
+
+eventHub.addEventListener("click", (eventObject) => {
+  console.log("you clicked me")
+  console.log(eventObject.target.id)
+  if(eventObject.target.id.startsWith("deleteNote")){
+    const idToDelete = eventObject.target.id.split("--")[1]
+    deleteNote(idToDelete)
+    .then(NoteList)
+  }
+})
 
 document.querySelector("#notes-nav-link").addEventListener("click", () => {
   noteListHTML();
